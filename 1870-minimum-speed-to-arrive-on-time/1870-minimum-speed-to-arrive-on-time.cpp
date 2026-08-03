662fb093e0bb45551c0119208edd2e2c20c91dc5
NitinKumai <Nitinkumai0134@gmail.com>
2026-08-03 22:13:18 +0530

    Time: 75 ms (49.26%), Space: 105.6 MB (23.7%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
 bool CanReach(vector<int>& dist, double hour, int speed){
-     double total_time = 0;
-     int n = dist.size();
-   for(int i = 0; i < dist.size() - 1; i++){
-       
-        total_time += ceil((double)dist[i]/speed) ;
-   }
-        total_time += (double)dist[n-1] / speed;
-        
-     return total_time <= hour;
+    double time = 0;
+    int n = dist.size();
+    for(int i = 0; i < n-1; i++){
+        time += ceil((double)dist[i] / speed);
+    }
+    time += (double)dist[n-1]/speed;
+    return time <=hour;
+
+
 }
     int minSpeedOnTime(vector<int>& dist, double hour) {
         int low = 1;
-        int high = 10000000;
-        int ans = low;
-        if(hour <= dist.size()-1)
-    return -1;
-        while ( low <=high){
+        int high = 1e7;
+        int ans = -1;
+        while(low<=high){
             int mid = low + (high - low)/2;
-            if (CanReach(dist, hour,mid)){
+            if(CanReach(dist, hour, mid)){
                 ans = mid;
-              high = mid - 1;
+                high = mid - 1;
             }
-                   else {
+            else {
                 low = mid + 1;
-                   }
+            }
         }
-   return ans;
+       return ans; 
     }
- 
 };
