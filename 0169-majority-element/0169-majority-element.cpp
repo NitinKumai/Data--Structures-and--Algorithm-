14bafadee13d8a1354f16554eb7b8fc682c30661
NitinKumai <Nitinkumai0134@gmail.com>
2026-08-19 20:27:59 +0530

    Time: 6 ms (12.2%), Space: 28.4 MB (14.81%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int>count;
+        for(int i = 0 ; i < nums.size();i++){
+            count[nums[i]]++;
+        }
+        for(auto it : count){
+            if(it.second > nums.size()/2){
+                return it.first;
+            }
+
+        }
+        return - 1;
+    }
+};
