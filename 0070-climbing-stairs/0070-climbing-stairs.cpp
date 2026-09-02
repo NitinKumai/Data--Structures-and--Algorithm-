79108c057222f29d27ae7ab3a8ea738255137e9f
NitinKumai <Nitinkumai0134@gmail.com>
2026-09-02 12:53:03 +0530

    Time: 0 ms (100%), Space: 8.6 MB (20.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int climbStairs(int n) {
-      if(n<=3) return n;
+      if(n<=2) return n;
       vector<int>arr(n+1);
-      arr[0] = 0;
       arr[1] = 1;
       arr[2] = 2;
       for(int i = 3 ; i <= n ; i++){
