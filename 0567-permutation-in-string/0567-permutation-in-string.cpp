0d2681fbc6611c4e52f8376b771a77fa00591c2c
NitinKumai <Nitinkumai0134@gmail.com>
2026-09-18 19:49:31 +0530

    Time: 3 ms (65.91%), Space: 11 MB (15.99%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -4,14 +4,13 @@ public:
         unordered_map<char,int>need;
         unordered_map<char,int>window;
         int left = 0;
-        if(s1.length() > s2.length()) return false;
-        for(char ch : s1)
-{
-    need[ch]++;
-}
-        for(int right = 0; right < s2.length();right++){
+        if(s1.length()>s2.length()) return false;
+        for(char ch : s1){
+            need[ch]++;
+        }
+        for(int right = 0 ; right < s2.length();right++){
             window[s2[right]]++;
-            if((right - left +1) > s1.length()){
+            if((right - left +1)>s1.length()){
                 window[s2[left]]--;
                 if(window[s2[left]]==0){
                     window.erase(s2[left]);
@@ -19,11 +18,10 @@ public:
                 left++;
             }
             if((right - left + 1)==s1.length()){
-                
-                if(window==need){
-                return true;
-            }
-            }
+                if(window == need){
+                    return true;
+                }
+        }
         }
         return false;
     }
